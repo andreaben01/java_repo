200debd9b1c458b3fc3010ba1c242af4a8329394
Andreabe <andylaboluda@gmail.com>
2023-03-04 09:50:56 -0300

    java ejercicios

---
+++ JavaProject/practicaJava/src/practicaJava/clases.java
@@ -0,0 +1,15 @@
+package practicaJava;
+
+public class clases {
+    // Atributos de la clase
+    String nombre;
+    int edad;
+    
+    // Métodos de la clase
+    public void saludar() {
+        System.out.println("Hola, mi nombre es " + nombre);
+    }
+    
+    
+}
+
---
+++ JavaProject/projectJava/src/projectJava/String_ejemplos.java
@@ -0,0 +1,34 @@
+package projectJava;
+
+public class String_ejemplos {
+
+	public static void main(String[] args) {
+
+		String x1 = "hola";
+		String x2 = "chau";
+		
+		System.out.println("concatenar");
+		String x3 = x1 + x2;
+		String x4 = x1 + " " + x2;
+		System.out.println(x3);
+		System.out.println(x4);
+
+
+		System.out.println("comparaciones");
+		System.out.println(x1.equals(x2));
+		System.out.println(x1.equals(x1));
+
+		
+		System.out.println("minus y mayus");
+		String unTexto = "laLa";
+		System.out.println(unTexto.toLowerCase());
+		System.out.println(unTexto.toUpperCase());
+
+
+		
+		
+	
+		
+	}
+
+}
